e4615e6abf9c3bc767ce29a5aad5b9b485c3b8bc
PEREGU <jcperegumma@gmail.com>
2022-11-26 17:06:32 -0500

    Subido correctamente el 7

--- Checkpoint/07.js
+++ Checkpoint/07.js
@@ -17,6 +17,21 @@
 function ordenarPrecios(arr) {
     // Tu código aquí:
 
+    var flag = true;
+    while (flag) {
+      flag = false;
+      for (let i=0; i< arr.length;i++){
+        if (arr[i] === 0) return false;
+        if (arr[i] > arr[i+1]){
+          let aux = arr[i];
+          arr [i] = arr [i+1];
+          arr[i+1]=aux;
+          flag = true;
+        }
+      }
+    }
+    return arr;       
+
 }
 // ⚠️ NO MODIFICAR NADA POR DEBAJO DE ESTA LÍNEA ⚠️
 module.exports = {
